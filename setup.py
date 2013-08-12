6c98c3bcfa608015f73c9eefd39aaea7ddf451e7
Marc Brinkmann <git@marcbrinkmann.de>
2013-08-12 12:23:44 +0200

    Release version 0.2.1.
    (commit by unleash 0.5)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='ngtpl',
-    version='0.3.dev1',
+    version='0.2.1',
     description='Collect angular templates, outputs compiled html',
     #long_description=read('README.rst'),
     author='Marc Brinkmann',
